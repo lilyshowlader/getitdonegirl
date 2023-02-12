2d794508be3535811e0baa38b375b3d1fdc0491f
lilyshowlader <lilyshowlader@gmail.com>
2023-02-12 18:15:49 -0500

    add create controller function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,13 @@
 import { Task } from '../models/task.js'
+// importing model info from our database
 
-
-// this controller function displays all of the tasks (the index view)
+// inside the index controller, use the Task model to request/query for ALL tasks within the database
 function index(req, res) {
   Task.find({})
   .then(tasks => { // tasks represents the result of the query, in this case ALL tasks
     res.render('tasks/index', {
       tasks: tasks,
+      time: req.time
     })
   })
   .catch(error => { // If there's an error, console.log it and redirect back home!
@@ -14,6 +15,9 @@ function index(req, res) {
     res.redirect('/')
   })
 }
+// above [{}] -> stands for find ALL documents. if you pass something into the object, it acts like a filter. since there is nothing in there, nothing is being filtered
+
+
 
 // this controller function shows the view page to create a new task
 
@@ -22,9 +26,10 @@ function newTask (req, res) {
 }
 
 // this controller function actually creates the new task
-
+// the function below using mongoose method model.create or task.create allows the user to create & post the new task
+// task.create is creating the task
 function create (req, res) {
-   console.log(req.body)
+  console.log(req.body)
   Task.create(req.body)
   .then(task => {
     res.redirect('/tasks')
